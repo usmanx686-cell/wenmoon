47a31bc162b2ca8921127c9716478cfd2fada602
usmanx686-cell <usmanx686@gmail.com>
2026-01-25 06:35:47 -0800

    Update IPAddress.js

--- src/models/IPAddress.js
+++ src/models/IPAddress.js
@@ -1,1 +1,41 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const IPAddress = sequelize.define('IPAddress', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  ip_address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  user_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  country_code: {
+    type: DataTypes.STRING
+  },
+  city: {
+    type: DataTypes.STRING
+  },
+  is_blocked: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  block_reason: {
+    type: DataTypes.STRING
+  },
+  last_activity: {
+    type: DataTypes.DATE
+  }
+}, {
+  tableName: 'ip_addresses',
+  timestamps: true,
+  underscored: true
+});
+
+module.exports = IPAddress;
